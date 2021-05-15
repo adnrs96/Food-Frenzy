6caf0db50413243dbd18cfc0e27eed2b33a7e9b4
Aditya Bansal <adi.bansal241996@gmail.com>
2021-05-15 22:02:35 +0530

    app_frenzy/db: implement connecting with database via session class.

---
+++ app_frenzy/db.py
@@ -0,0 +1,12 @@
+from app_frenzy.config import get_app_frenzy_settings
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+settings = get_app_frenzy_settings()
+
+engine = create_engine(settings.DATABASE_CONN_STR)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+AppFrenzyBase = declarative_base()
