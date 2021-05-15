d59228b5bce2872d14d642bd88a5390b46288015
Aditya Bansal <adi.bansal241996@gmail.com>
2021-05-15 22:04:00 +0530

    app_frenzy/models: implement ORM def. for Restaurant, User & supporting tables.

---
+++ app_frenzy/models.py
@@ -0,0 +1,62 @@
+from app_frenzy.db import AppFrenzyBase
+
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    Float,
+    ForeignKey,
+    Integer,
+    String,
+    Time,
+)
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import Index
+
+
+class Restaurant(AppFrenzyBase):
+    __tablename__ = "restaurant"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    cash_balance = Column(Float)
+    menu = relationship("MenuItem")
+    timings = relationship("RestaurantTiming")
+
+
+class MenuItem(AppFrenzyBase):
+    __tablename__ = "menu_item"
+    id = Column(Integer, primary_key=True)
+    restaurant = Column(Integer, ForeignKey("restaurant.id"))
+    dish_name = Column(String)
+    price = Column(Float, index=True)
+
+
+class RestaurantTiming(AppFrenzyBase):
+    __tablename__ = "restaurant_timing"
+    __table_args__ = (
+        Index("restaurant_timing_idx", "day", "opens", "closes"),
+    )
+    id = Column(Integer, primary_key=True)
+    restaurant = Column(Integer, ForeignKey("restaurant.id"))
+    day = Column(String)
+    opens = Column(Time)
+    closes = Column(Time)
+
+
+class User(AppFrenzyBase):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    str_id = Column(String, index=True)
+    name = Column(String)
+    cash_balance = Column(Float)
+    purchase_history = relationship("UserTransaction")
+
+
+class UserTransaction(AppFrenzyBase):
+    __tablename__ = "user_transactions"
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer, ForeignKey("user.id"))
+    restaurant = Column(Integer, ForeignKey("restaurant.id"))
+    menu_item = Column(Integer, ForeignKey("menu_item.id"))
+    transaction_amount = Column(Float)
+    transaction_date = Column(DateTime)
