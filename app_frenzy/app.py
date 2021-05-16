62839e81dcb61b5a1d30e556ed01e5faf145ab01
Aditya Bansal <adi.bansal241996@gmail.com>
2021-05-16 11:34:10 +0530

    app_frenzy/app: update root api handler to act as app health endpoint.
    This is useful to do liveness checks.

--- app_frenzy/app.py
+++ app_frenzy/app.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/health")
+async def health():
+    return {"message": "Alive!"}
